6ab265849e8a445c9986b5d9ac20d54607fc3089
chethanadukkipati <31363876+chethanadukkipati@users.noreply.github.com>
2017-10-12 21:22:44 -0500

    Update Testcase2.java

--- src/test/java/Group18/Eventbrite/Testcase2.java
+++ src/test/java/Group18/Eventbrite/Testcase2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Group18.Eventbrite;
 
 import static org.junit.Assert.*;
@@ -15,21 +14,3 @@ public class Testcase2 {
 	}
 
 }
-=======
-package Group18.Eventbrite;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-public class Testcase2 {
-
-	@Test
-	public void test2()
-	{
-		RegistrationPage rp=new RegistrationPage();
-		String output=rp.register();
-		assertEquals("Registration Successfull",output);
-	}
-
-}
->>>>>>> e055a7ace2af7f9e085f1fac30d0fc8b5bf0bf90
