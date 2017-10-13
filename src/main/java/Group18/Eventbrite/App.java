891a905f7c22e7db37ea00e26045d4cb2cd5b1be
chethanadukkipati <31363876+chethanadukkipati@users.noreply.github.com>
2017-10-12 21:29:41 -0500

    Update App.java

--- src/main/java/Group18/Eventbrite/App.java
+++ src/main/java/Group18/Eventbrite/App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Group18.Eventbrite;
 
 /**
@@ -12,18 +11,4 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
-=======
-package Group18.Eventbrite;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
->>>>>>> e055a7ace2af7f9e085f1fac30d0fc8b5bf0bf90
