dc660938b494f2c6d9469d68027e6cce66054e0c
chethanadukkipati <31363876+chethanadukkipati@users.noreply.github.com>
2017-10-12 21:23:12 -0500

    Update Testcase3.java

--- src/test/java/Group18/Eventbrite/Testcase3.java
+++ src/test/java/Group18/Eventbrite/Testcase3.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Group18.Eventbrite;
 
 import static org.junit.Assert.*;
@@ -18,24 +17,4 @@ public class Testcase3 {
 	}
 
 }
-=======
-package Group18.Eventbrite;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import java.util.*;
-public class Testcase3 {
-
-	@Test
-	public void test() {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Select requied operation: \n Ticket Booking \n Create an event");
-		String choice=sc.nextLine();
-		LoginPage lp=new LoginPage();
-		String output=lp.loginpagefunction(choice,1);
-		assertEquals("Ticket Booked",output);
-	}
-
-}
->>>>>>> e055a7ace2af7f9e085f1fac30d0fc8b5bf0bf90
